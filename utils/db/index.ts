765e5e5c87d90b073b2fc08c16619ac303cad159
Nick Johnson <arachnid@notdot.net>
2022-12-07 16:56:25 +1300

    Get service account from environment

--- utils/db/index.ts
+++ utils/db/index.ts
@@ -1,10 +1,9 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../../serviceAccountKey.json';
 
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+      credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '') as admin.ServiceAccount)
     });
   } catch (error) {
     console.log('Firebase admin initialization error', (error as Error).stack);
