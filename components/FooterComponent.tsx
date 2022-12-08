1995f51dff2913233508d2e2a7ed86015c1a0f38
Nick Johnson <arachnid@notdot.net>
2022-12-09 10:57:00 +1300

    Added disclaimer to footer

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -5,7 +5,8 @@ function FooterComponent() {
     <footer className={styles.footer}>
         <p>
             <a href='https://twitter.com/lore_genie' target='_blank' rel="noreferrer">@lore_genie</a><br />
-            Footer here
+            All AI-generated content is Copyright Lore Genie and licensed <a href="https://creativecommons.org/licenses/by/2.0/">CC-BY</a>.
+            Note that we may use content you generate with Lore Genie to train and improve future versions of the system.
         </p>
     </footer>
   </>;
