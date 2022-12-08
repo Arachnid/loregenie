4fa1d3afe7c2c7ac6a8c8764e726906c52ee8904
Dom Perri <hello@dperri.com>
2022-12-08 13:35:15 -0800

    Fixed ts error in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import HeaderComponent from '../components/HeaderComponent';
 import FooterComponent from '../components/FooterComponent';
+import { AppProps } from 'next/app';
 
-const Layout = ({ children }) => <div className="layout">{children}</div>
+const Layout = ({ children } : { children: React.ReactNode }) => <div className="layout">{children}</div>
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }:AppProps) {
   return (
     <Layout>
       <HeaderComponent />
