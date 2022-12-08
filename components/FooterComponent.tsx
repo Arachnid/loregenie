23edc93367f9d43baf7e753edfc5d36dae7a9ab9
Nick Johnson <arachnid@notdot.net>
2022-12-09 11:09:37 +1300

    Add analytics, tweak css, tweak copyright

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -5,7 +5,7 @@ function FooterComponent() {
     <footer className={styles.footer}>
         <p>
             <a href='https://twitter.com/lore_genie' target='_blank' rel="noreferrer">@lore_genie</a><br />
-            All AI-generated content is Copyright Lore Genie and licensed <a href="https://creativecommons.org/licenses/by/2.0/">CC-BY</a>.
+            All AI-generated content is Copyright 2022 Lore Genie and licensed <a href="https://creativecommons.org/licenses/by/2.0/">CC-BY</a>.
             Note that we may use content you generate with Lore Genie to train and improve future versions of the system.
         </p>
     </footer>
--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -1,8 +1,20 @@
 import styles from '../styles/Header.module.css';
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
 
 function HeaderComponent() {
   return <>
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-D8EPPREN3N" strategy='afterInteractive'/>
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-D8EPPREN3N');
+      `}
+    </Script>
     <header className={styles.header}>
         <h1>
             <Link href="/">
